d3fc2473892fc23c4d19c0e364f1dba24f80f175
Marconys <marconysbatera@gmail.com>
2022-12-12 16:33:17 -0300

    created project

---
+++ ProjetoCanalAlunoti91/Classes/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCanalAlunoti91.Classes
+{
+    internal class Aluno
+    {
+        //Atributos/Campos
+        private int _id;
+        private string _name;
+        private string _email;
+        private string _cpf;
+        private DateTime _dataNasc;
+        private int _idCurso;
+
+        //Propriedades
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+        public string Cpf { get { return _cpf; } set { _cpf = value; } }
+        public DateTime DataNasc { get { return _dataNasc; } set { _dataNasc = value; } }
+        public int IdCurso { get { return _idCurso; } set { _idCurso = value; } }
+
+
+
+    }
+}
---
+++ ProjetoCanalAlunoti91/Classes/Cursos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCanalAlunoti91.Classes
+{
+    internal class Cursos
+    {
+    }
+}
